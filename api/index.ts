740037fac1ab73acc39e4a3da97ed2d739eafbfd
Daniel Pinzón <102686918+DanSeph08@users.noreply.github.com>
2023-04-13 16:42:00 -0500

    Starting api

---
+++ api/index.ts
@@ -0,0 +1,5 @@
+import app from './src/App';
+
+app.listen(3001, () => {
+    console.log('listening on port 3001');
+});
---
+++ api/src/App.ts
@@ -0,0 +1,38 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import config from '../lib/config';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(cors({
+    origin: config.cors,
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+}));
+
+interface error{
+    status: number;
+    message: string;
+}
+
+app.use((err:error, req: Request, res:Response, next:NextFunction) => {
+	// eslint-disable-line no-unused-vars
+	const status = err.status || 500;
+	const message = err.message || err;
+	console.log(err);
+	res.status(status).send(message);
+});
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello TypeScript');
+});
+
+export default app;

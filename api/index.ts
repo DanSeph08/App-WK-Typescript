334b12a75a9d5c5564fcc39db62d81f34034209c
Daniel Pinzón <102686918+DanSeph08@users.noreply.github.com>
2023-04-17 22:09:05 -0500

    Back Finally

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,10 @@
 import server from './src/App'
+import { sequelize } from './src/DB';
 
-server.listen(3001, () => {
-    console.log('listening on port 3001');
-});
+sequelize.sync({ force: true, logging: false }).then(() => {
+    console.log('DB is connect 💪');
+    server.listen(3001, () => {
+        console.log('listening on port 3001');
+    });
+})
+.catch((err) => console.error(err));
--- api/lib/config.ts
+++ api/lib/config.ts
@@ -3,15 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    dbUser: process.env.DB_USER || 'postgres',
-    dbPassword: process.env.DB_PASS || '1234',
-    dbHost: process.env.DB_HOST || 'localhost',
-    dbName: process.env.DB_NAME || 'wk-TS',
-    dbPort: process.env.DB_PORT || '5432',
-    dev: process.env.NODE_ENV !== 'production',
-    port: process.env.API_PORT || '3001',
-    host: process.env.API_host || 'localhost',
-    cors: process.env.CORS || 'localhost:3000',
+	dbUser: process.env.DB_USER || 'postgres',
+	dbPassword: process.env.DB_PASSWORD || '081217',
+	dbHost: process.env.DB_HOST || 'localhost',
+	dbName: process.env.DB_NAME || 'postgres',
+	dbPort: process.env.DB_PORT || '5432',
+	dev: process.env.NODE_ENV !== 'production',
+	port: process.env.API_PORT || '3001',
+	host: process.env.API_HOST || 'localhost',
+	cors: process.env.CORS || 'localhost:3000',
 };
 
 export default config;

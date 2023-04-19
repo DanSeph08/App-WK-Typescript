7059e087408932ef40d9e8a62d522cd479ef7cfc
Daniel Pinzón <102686918+DanSeph08@users.noreply.github.com>
2023-04-19 17:45:01 -0500

    Advance

---
+++ client/src/redux/reducer/usersReducer.tsx
@@ -0,0 +1,15 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState = {
+
+// };
+
+// export const users = createSlice({
+//     name: 'users',
+//     initialState,
+//     reducer: {
+//         allUsers: (state, action) => {
+//             state.users = action.users
+//         },
+//     }
+// });
